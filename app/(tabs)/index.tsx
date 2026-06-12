4658283d0dc64b4c6eaeb9c6fa3242a9e167f155
FyyFzn <rafifauzanhermansah@gmail.com>
2026-06-13 00:31:50 +0700

    revert

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -169,7 +169,7 @@ export default function BerandaScreen() {
         <Text style={styles.logoText}>
           WIBU<Text style={styles.logoAccent}>FLIX</Text>
         </Text>
-        <Text style={{ color: Colors.textMuted, fontSize: 11, fontWeight: 'bold', marginLeft: 8, marginTop: 4 }}>v1.3</Text>
+        <Text style={{ color: Colors.textMuted, fontSize: 11, fontWeight: 'bold', marginLeft: 8, marginTop: 4 }}>v1.4</Text>
       </View>
       <View style={styles.headerLine} />
 
